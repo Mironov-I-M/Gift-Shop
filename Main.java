85bc152e854b54e31dd3e50933d57001640554a5
Mironov-I-M <takeda.mihailovich@gmail.com>
2025-10-23 22:11:40 +0300

    removed unused equals

--- Main.java
+++ Main.java
@@ -23,7 +23,7 @@ public class Main {
             if (prices[middle] <= money) {
                 left = middle + 1;
             }
-            if (equals && prices[middle] > money) {
+            if (prices[middle] > money) {
                 right = middle - 1;
             }
             if (!equals && prices[middle] > money) {
@@ -32,4 +32,5 @@ public class Main {
         }
         return 0;
     }
+
 }
